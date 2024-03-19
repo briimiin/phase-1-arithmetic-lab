be7a39b8b606fc7b88777b90e448aef1c1280915
briimiin <kipkorirbrimin005@gmail.com>
2024-03-19 14:48:53 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
-// Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+console.log(multiply);
+let random = Math.floor(Math.random()*
+Number.MAX_SAFE_INTEGER)+1;
+console.log(random);
+let num3=14;
+let num4=5;
+let mod=num3%num4;
+console.log(mod);
+let num5=10;
+let num6=5;
+let num7=20;
+let num8=2;
+let max=Math.max(num5,num6,num7,num8);
+console.log(max);
